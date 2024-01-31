f473ebd2cb0373a5a7ed1d612914e86e2bc376c2
officialAbhinav <official.abhinavsingh15@gmail.com>
2024-01-31 13:53:22 +0530

    Version to 2.0.0, improve validation

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# secure-password-check
+# Secure Password Check
+
+[**A simple npm package to check the validity on the basis of strength of passwords based on various parameters.**](https://www.npmjs.com/package/secure-password-check "visit npmjs.com")
+
+
+## Installation
+
+Use the following npm command to install the package:
+
+```bash
+npm install secure-password-check
+```
+
+
+## Validation
+
+The password validity on the basis of strength is checked based on the following criteria:
+
+- Minimum length
+- Maximum length
+- Special characters
+- Numbers
+- Uppercase characters
+- Lowercase characters
+- White spaces
+
+
+## Usage
+
+```bash
+import { checkPassword } from 'secure-password-check';  
+const result = checkPassword('YourPassword123!');
+console.log(result);
+```
+
+
+## Parameters
+
+The checkPassword function accepts the following parameters:
+
+- **`string` (default: "") :**  The password to be checked.
+- **`minLength` (default: 8) :** Minimum length required for the password.
+- **`maxLength` (default: 20) :** Maximum length allowed for the password.
+- **`isSpecialCharAllowed` (default: true) :** Whether special characters are allowed.
+- **`isNumberAllowed` (default: true) :** Whether numbers are allowed.
+- **`isAllUpperCaseAllowed` (default: false) :** Whether all uppercase characters are allowed.
+- **`isAllLowerCaseAllowed` (default: false) :** Whether all lowercase characters are allowed.
+- **`isWhiteSpaceAllowed` (default: false) :** Whether white spaces are allowed.
+
+
+# Examples
+
+```
+const result1 = checkPassword('StrongP@ss');
+console.log(result1);
+// Output: { ans: true, description: 'Valid Password' }
+
+const result2 = checkPassword('weak');
+console.log(result2);
+// Output: { ans: false, description: 'Not a valid password, minimum length 8 is required' }
+```
+
+## Contributing
+Contributions are welcome! If you find any bugs or have suggestions for new features, feel free to open an issue or submit a pull request.
+
+## License
+ISC Licensed. Abhinav Singh, 2024.
+
--- src/index.js
+++ src/index.js
@@ -1,39 +1,44 @@
-export const checkPassword = (str) => {
+export const checkPassword = (str = "", minLength = 8, maxLength = 20, isSpecialCharAllowed = true, isNumberAllowed = true, isAllUpperCaseAllowed = false, isAllLowerCaseAllowed = false, isWhiteSpaceAllowed = false) => {
 
     const string = str.toString();
-    const resObj = {ans : true, description: "Valid Password"};
+    const respObj = {ans : true, description: "Valid Password"};
 
-    if(string.length < 8){
-        resObj.ans = false;
-        resObj.description = "Not a valid password, minimum length 8 is required";
-        return resObj;
+    if(string.length < minLength){
+        respObj.ans = false;
+        respObj.description = "Not a valid password, minimum length 8 is required";
+        return respObj;
     }
-    else if(string.includes(" ")){
-        resObj.ans = false;
-        resObj.description = "Not a valid password, Space is not required";
-        return resObj;
+    else if(string.length > maxLength){
+        respObj.ans = false;
+        respObj.description = "Not a valid password, maximum length allowed is 16";
+        return respObj;
     }
-    else if(!(/[a-z]/.test(string))){
-        resObj.ans = false;
-        resObj.description = "Not a valid password, use some lowercase  alphabets";
-        return resObj;
+    else if(!isWhiteSpaceAllowed && string.includes(" ")){
+        respObj.ans = false;
+        respObj.description = "Not a valid password, Space is not required";
+        return respObj;
     }
-    else if(!(/[A-Z]/.test(string))){
-        resObj.ans = false;
-        resObj.description = "Not a valid password, use some uppercase alphabets";
-        return resObj;
+    else if(!isAllLowerCaseAllowed && !(/[a-z]/.test(string))){
+        respObj.ans = false;
+        respObj.description = "Not a valid password, use some lowercase  alphabets";
+        return respObj;
     }
-    else if(!(/[0-9]/.test(string))){
-        resObj.ans = false;
-        resObj.description = "Not a valid password, use some numbers";
-        return resObj;
+    else if(!isAllUpperCaseAllowed && !(/[A-Z]/.test(string))){
+        respObj.ans = false;
+        respObj.description = "Not a valid password, use some uppercase alphabets";
+        return respObj;
     }
-    else if(!(/[@#$&%!]/.test(string))){
-        resObj.ans = false;
-        resObj.description = "Not a valid password, use some valid special charecters";
-        return resObj;
+    else if(isNumberAllowed && !(/[0-9]/.test(string))){
+        respObj.ans = false;
+        respObj.description = "Not a valid password, use some numbers";
+        return respObj;
     }
-    return resObj;
+    else if(isSpecialCharAllowed && !(/[@#$&%!]/.test(string))){
+        respObj.ans = false;
+        respObj.description = "Not a valid password, use some valid special charecters";
+        return respObj;
+    }
+    return respObj;
 }
 
 
